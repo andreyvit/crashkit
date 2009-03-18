9e08e862ee81704556967c415c90f12047fe0a95
Andrey Tarantsov <andreyvit@gmail.com>
2009-03-18 07:00:05 +0600

    PHP client integration instructions.

--- php-client/crashkit-demo.php
+++ php-client/crashkit-demo.php
@@ -1,5 +1,13 @@
 <?php
   define('CRASHKIT_PRODUCT', 'test/php');
+  define('CRASHKIT_DIE_MESSAGE',
+    '<title>Server error</title>' .
+    '<div style="width: 600px; margin: 50px auto 0px auto; font: 14px Verdana, sans-serif;">' .
+    '<b style="color: red;">Server error</b>' .
+    '<p>Sorry, we have failed to process your request now. Please try again later.' .
+    '<p>Our developers have just been notified about this error.' .
+    '<p>If you need help immediately, please e-mail <a href="mailto:test@example.com">test@example.com</a>.' .
+    '</div>');
   require_once 'crashkit.php';
 ?>
 <?
